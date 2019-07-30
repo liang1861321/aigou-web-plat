65fad8827bac945b0e227c95affa8506e7eac82e
liang1861321 <1773569548@qq.com>
2019-07-30 08:45:02 +0800

    完成登录前后联调

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -16,63 +16,72 @@
 </template>
 
 <script>
-  import { requestLogin } from '../api/api';
-  //import NProgress from 'nprogress'
-  export default {
-    data() {
-      return {
-        logining: false,
-        ruleForm2: {
-          account: 'admin',
-          checkPass: '123456'
+    //import { requestLogin } from '../api/api';
+    //import NProgress from 'nprogress'
+    export default {
+        data() {
+            return {
+                logining: false,
+                ruleForm2: {
+                    account: 'admin',
+                    checkPass: '123456'
+                },
+                rules2: {
+                    account: [
+                        { required: true, message: '请输入账号', trigger: 'blur' },
+                        //{ validator: validaePass }
+                    ],
+                    checkPass: [
+                        { required: true, message: '请输入密码', trigger: 'blur' },
+                        //{ validator: validaePass2 }
+                    ]
+                },
+                checked: true
+            };
         },
-        rules2: {
-          account: [
-            { required: true, message: '请输入账号', trigger: 'blur' },
-            //{ validator: validaePass }
-          ],
-          checkPass: [
-            { required: true, message: '请输入密码', trigger: 'blur' },
-            //{ validator: validaePass2 }
-          ]
-        },
-        checked: true
-      };
-    },
-    methods: {
-      handleReset2() {
-        this.$refs.ruleForm2.resetFields();
-      },
-      handleSubmit2(ev) {
-        var _this = this;
-        this.$refs.ruleForm2.validate((valid) => {
-          if (valid) {
-            //_this.$router.replace('/table');
-            this.logining = true;
-            //NProgress.start();
-            var loginParams = { username: this.ruleForm2.account, password: this.ruleForm2.checkPass };
-            requestLogin(loginParams).then(data => {
-              this.logining = false;
-              //NProgress.done();
-              let { msg, code, user } = data;
-              if (code !== 200) {
-                this.$message({
-                  message: msg,
-                  type: 'error'
+        methods: {
+            handleReset2() {
+                this.$refs.ruleForm2.resetFields();
+            },
+            //登录
+            handleSubmit2(ev) {
+                var _this = this;
+                this.$refs.ruleForm2.validate((valid) => {
+                    if (valid) {
+                        //_this.$router.replace('/table');
+                        this.logining = true;
+                        //NProgress.start();
+                        var loginParams = { username: this.ruleForm2.account, password: this.ruleForm2.checkPass };
+                        //自己使用axios发送ajax请求
+                        this.$http.post('/plat/login',loginParams)
+                            .then((res)=>{
+                                console.debug(res)
+                                this.logining = false;
+                                //NProgress.done();
+                                //回调参数的data属性才是响应的数据
+                                let { message, errorCode, restObj,success } = res.data;
+                                if (!success) {
+                                    this.$message({
+                                        message: message,
+                                        type: 'error'
+                                    });
+                                } else {
+                                    //把用户信息存到了session中
+                                    sessionStorage.setItem('user', JSON.stringify(restObj));
+                                    //页面跳转
+                                    this.$router.push({ path: '/echarts' });
+                                }
+                            })
+
+
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
                 });
-              } else {
-                sessionStorage.setItem('user', JSON.stringify(user));
-                this.$router.push({ path: '/table' });
-              }
-            });
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      }
+            }
+        }
     }
-  }
 
 </script>
 
